bf6b84707c1d0ab98bb5155d20e8e2fb1c3be39c
Shamima <shamima.hossain19@gmail.com>
2024-06-09 01:01:41 +0600

    Add files for landmark classification

---
+++ Landmark_classification_and_tagging_for_social_media/src/create_submit_zip.py
@@ -0,0 +1,39 @@
+import datetime
+import glob
+import subprocess
+import zipfile
+
+def create_submit_pkg():
+
+    # Source files
+    src_files = glob.glob("src/*.py")
+
+    # Notebooks
+    notebooks = glob.glob("*.ipynb")
+
+    # Generate HTML files from the notebooks
+    for nb in notebooks:
+        cmd_line = f"jupyter nbconvert --to html {nb}"
+
+        print(f"executing: {cmd_line}")
+        subprocess.check_call(cmd_line, shell=True)
+
+    html_files = glob.glob("*.htm*")
+
+    now = datetime.datetime.today().isoformat(timespec="minutes").replace(":", "h")+"m"
+    outfile = f"submission_{now}.zip"
+    print(f"Adding files to {outfile}")
+    
+    with zipfile.ZipFile(outfile, 'w') as zipf:
+        for name in (src_files + notebooks + html_files):
+            print(name)
+            zipf.write(name)
+
+    print("")
+    msg = f"Done. Please submit the file {outfile}"
+    print("-" * len(msg))
+    print(msg)
+    print("-" * len(msg))
+
+if __name__ == "__main__":
+    create_submit_pkg()
